591a07e23876c54640984847cd57c973c57dec34
Prince <princesaluja102@gmail.com>
2024-03-14 12:34:36 +0530

    both file added

---
+++ script1.js
@@ -0,0 +1,35 @@
+
+
+name();
+doSomething();
+
+fullName = "Harshit" ;
+console.log(fullName);
+
+
+
+
+const user = {
+    name : "Prince" ,
+    age : 25
+} 
+
+console.log(user);
+console.log(typeof user);
+
+
+function name() {
+    console.log("Prince Saluja");
+}
+
+var fullName = "Garv";
+
+
+function doSomething() {
+
+ 
+    // x=30;
+    console.log(x) ;
+    var x = 25 ;
+
+}

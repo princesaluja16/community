cac1be3ff178bad38e24bfc19d36156619fd4396
Prince <princesaluja102@gmail.com>
2024-03-16 14:48:39 +0530

    Call added

--- script1.js
+++ script1.js
@@ -26,10 +26,31 @@ var fullName = "Garv";
 
 
 function doSomething() {
-
- 
     // x=30;
     console.log(x) ;
     var x = 25 ;
 
 }
+
+// call , apply , bind
+
+function greet(hobby) {
+    console.log(`Hello Mr. ${this.name} and his hobby is ${hobby}`);
+}
+
+const user1 = {
+    name : "Prince",
+    age : 26
+}
+
+const user2 = {
+    name : "Harshit",
+    age : 30
+}
+
+greet.call(user2 , "Singing");
+
+greet.apply(user1 , ["Dancing"]);
+
+const bind = greet.bind(user1 , "Smoking");
+bind();
